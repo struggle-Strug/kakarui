89072f104a6528f0c8f0f68591c16a5aa9259506
Thang Pham <pqtsoft@musu-be.com>
2024-07-25 08:11:56 +0000

    Merged PR 338: first deployment

---
+++ src/services/placeholder-data/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+import deployment_detail from './deployment_detail.json'
+//
+import module from './module.json'
+import module_config from './module_config.json'
+import module_config_detail from './module_config_detail.json'
+import module_selection from './module_selection.json'
+import module_set from './module_set.json'
+import module_set_detail from './module_set_detail.json'
+import module_set_selection from './module_set_selection.json'
+import module_setting from './module_setting.json'
+//
+import user_by_organization from './user_by_organization.json'
+import users from './users.json'
+
+export const placeHolderData = {
+  deployment_detail,
+  //
+  users,
+  user_by_organization,
+  //
+  module_setting,
+  module_selection,
+  module_set_selection,
+  module_config_detail,
+  module_set_detail,
+  module_config,
+  module_set,
+  module,
+}
