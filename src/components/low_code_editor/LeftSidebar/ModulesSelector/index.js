0570037f330a2e8f9ee556a40a5c497c23a5ce3d
Wata-Naoki <watanabe.naoki.0129@gmail.com>
2024-10-25 11:15:18 +0900

    検索inputのバツボタンを押すと値がクリアされる実装追加

--- src/components/low_code_editor/LeftSidebar/ModulesSelector/index.js
+++ src/components/low_code_editor/LeftSidebar/ModulesSelector/index.js
@@ -1,10 +1,26 @@
 import { Divider, Input } from 'antd'
 
 import Image from 'next/image'
+import { useState } from 'react'
 
 import { Assets } from '@/constants'
 
 const ModulesSelector = ({ onDragStart, onFilter }) => {
+  // 入力値を管理する状態
+  const [inputValue, setInputValue] = useState('')
+
+  // 入力が変更された時の処理
+  const handleInputChange = (e) => {
+    const value = e.target.value
+    setInputValue(value)
+    onFilter(value) // フィルタ関数を呼び出す
+  }
+
+  // クリアボタンが押された時の処理
+  const handleClear = () => {
+    setInputValue('') // 入力値をクリア
+    onFilter('') // フィルタを空にする
+  }
   return (
     <div className="h-[250px] bg-white px-3 py-5">
       {/* 3つのモジュールアイコン */}
@@ -67,14 +83,14 @@ const ModulesSelector = ({ onDragStart, onFilter }) => {
         <div>
           <Input
             size=""
+            value={inputValue} // 入力値を状態から取得
             placeholder="スキル名・説明"
             className="border border-solid border-[#E3E3E4]"
-            color="red"
             prefix={
               <Image
                 src={Assets.LOWCODEEDITOR.search}
                 className="shrink-0"
-                alt="gen3p"
+                alt="search"
                 width={18}
                 height={18}
               />
@@ -82,13 +98,14 @@ const ModulesSelector = ({ onDragStart, onFilter }) => {
             suffix={
               <Image
                 src={Assets.LOWCODEEDITOR.closed}
-                className="shrink-0"
-                alt="gen3p"
+                className="cursor-pointer shrink-0"
+                alt="clear"
                 width={12}
                 height={12}
+                onClick={handleClear}
               />
             }
-            onChange={(e) => onFilter(e.target.value)}
+            onChange={handleInputChange}
           />
         </div>
       </div>
