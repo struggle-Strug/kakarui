caf3d0fac00002df5e43f571f59b4ab0f9fd2a47
Wata-Naoki <watanabe.naoki.0129@gmail.com>
2024-10-26 23:51:22 +0900

    最新のdevelopの_app.jsに合わせて修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,8 @@
-import { SessionProvider } from 'next-auth/react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect } from 'react'
+import { useCallback } from 'react'
 
-import { API, APP_NAME, Routes } from '@/constants'
-import notification_hub from '@/constants/notification_hub'
+import { APP_NAME, Routes } from '@/constants'
 
 import { Progressbar } from '@/components/layout/common'
 
@@ -12,9 +10,6 @@ import AppProviders from '@/contexts'
 import AuthLayout from '@/layouts/AuthLayout'
 import DashboardLayout from '@/layouts/DashboardLayout'
 
-import { buildApiURL } from '@/utils/helper/request'
-
-import { Axios } from '@/libs/axios'
 import '@/styles/index.css'
 
 export default function MyApp({ Component, pageProps }) {
@@ -38,66 +33,8 @@ export default function MyApp({ Component, pageProps }) {
 
       return <DashboardLayout>{children}</DashboardLayout>
     },
-    [isAuthPage, isPublicPage, router.pathname]
+    [isAuthPage, isPublicPage, idLowCodeEditorPage, idLowCodeEditorEditPage]
   )
-
-  function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
-    const rawData = window.atob(base64)
-    const outputArray = new Uint8Array(rawData.length)
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i)
-    }
-    return outputArray
-  }
-
-  useEffect(() => {
-    const vapidPublicKey = notification_hub.VAPID_Public_Key
-    //user entra ID
-    const user = localStorage.user
-    const entraId = user?.entraId
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      const registerServiceWorker = async () => {
-        try {
-          const registration = await navigator.serviceWorker.register('/sw.js')
-
-          // Subscribe to push notifications
-          const subscription = await registration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
-          })
-
-          const subscriptionData = {
-            endpoint: subscription.endpoint,
-            p256dh: btoa(
-              String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('p256dh')))
-            ),
-            auth: btoa(
-              String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('auth')))
-            ),
-          }
-
-          const response = await Axios.put(
-            buildApiURL(API.NOTIFICATION, { entra_id: entraId }),
-            JSON.stringify(subscriptionData),
-            {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            }
-          )
-
-          const data = await response.json()
-          console.log('Subscription response from server:', data)
-        } catch (error) {
-          console.error('Service Worker registration or subscription error:', error)
-        }
-      }
-
-      registerServiceWorker()
-    }
-  }, [])
 
   return (
     <>
@@ -109,12 +46,11 @@ export default function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </Head>
-      <SessionProvider session={session}>
-        <AppProviders locale={router.locale || 'ja'} pageProps={restPageProps}>
-          <Progressbar />
-          {layout(<Component {...restPageProps} />)}
-        </AppProviders>
-      </SessionProvider>
+      <AppProviders locale={router.locale || 'ja'} pageProps={restPageProps}>
+        <Progressbar />
+        {/* <ServiceWorker /> */}
+        {layout(<Component {...restPageProps} />)}
+      </AppProviders>
     </>
   )
 }
