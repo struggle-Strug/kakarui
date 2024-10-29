2add5671dcff613af9e0e6c11a00b2c69a6e48b1
monolab825 <p.smiles777@gmail.com>
2024-10-29 21:43:59 +0900

    update project_id and site_id

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -24,7 +24,7 @@ export default {
   SEQUENCE_CONFIG_CREATE: '/editor/[project_id]/[module_config_id]',
 
   PROJECT: '/project-manage',
-  PROJECT_DETAIL: '/project-manage/[project_id]',
+  PROJECT_DETAIL: '/project-manage/[project_id]/project-data',
 
   DEPLOY: '/deploy-manage',
 
