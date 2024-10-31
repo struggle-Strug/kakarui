c279b252085edaa87b6a830410a3aefa9ee510d8
Miku Suzuki <miku13.suzuki.bp@musu-be.com>
2024-10-31 10:33:28 +0900

    fix: SAS URLを使用したモジュール更新時に、ファイルアップロードが成功せず後続処理が実行されない不具合の修正

--- src/constants/api.js
+++ src/constants/api.js
@@ -40,7 +40,7 @@ export const API = {
     CREATEURL: '/organizations/{organization_id}/modules', // POST
     CREATEUPLOAD: '{baseUrl}/{module_upload_id}_{architecture}.tar?{queryParams}', // PUT
     UPDATEURL: '/organizations/{organization_id}/modules/{module_id}', // PUT
-    UPDATEUPLOAD: '{baseUrl}/{module_upload_id}-{architecture}.tar?{queryParams}/{module_id}', // PUT
+    UPDATEUPLOAD: '{baseUrl}/{module_upload_id}_{architecture}.tar?{queryParams}/{module_id}', // PUT
     DELETE: '/organizations/{organization_id}/modules/{module_id}', // DELETE
   },
 
